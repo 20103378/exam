a20bb4f36cccf05caeef41fed79cf576b6c80119
jiangna <jiangna20103378@sina.com>
2020-03-30 13:22:10 +0800

    第一次更改

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -10,7 +10,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(DemoApplication.class, args);
     }
 
